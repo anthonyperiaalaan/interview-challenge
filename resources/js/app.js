64d20840ef42b2cc1ef10e52425e85dadd6049b2
anthony <anthonyperiaalaan@gmail.com>
2021-09-13 23:35:44 +0800

    feat: setup vee-validate

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 
 require('./bootstrap');
+require('./plugins')
 
 Vue.config.productionTip = false
 
---
+++ resources/js/plugins/vee-validate.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import {extend, ValidationObserver, ValidationProvider,} from 'vee-validate'
+import {required} from 'vee-validate/dist/rules'
+
+extend('required', {
+    ...required,
+    message: 'This field is required.',
+})
+
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
