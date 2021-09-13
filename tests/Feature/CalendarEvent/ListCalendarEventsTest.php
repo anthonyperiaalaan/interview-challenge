da66e878ea2219f1c0e8175093a3746e8fc89b12
anthony <anthonyperiaalaan@gmail.com>
2021-09-13 22:47:23 +0800

    test: list calendar events

---
+++ tests/Feature/CalendarEvent/ListCalendarEventsTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature\CalendarEvent;
+
+use App\Models\CalendarEvent;
+use Arr;
+use Tests\TestCase;
+
+class ListCalendarEventsTest extends TestCase
+{
+    public function testListCalendarEvents()
+    {
+        /** @var \Illuminate\Database\Eloquent\Collection|CalendarEvent[] $events */
+        $events = CalendarEvent::factory()
+            ->count(3)
+            ->withDays()
+            ->create();
+
+        $this->getJson('/web-api/calendar-event')
+            ->assertSuccessful()
+            ->assertJsonStructure(
+                [
+                    'data' => [
+                        [
+                            'id',
+                            'title',
+                            'start_date',
+                            'end_date',
+                            'created_at',
+                            'updated_at',
+                        ],
+                    ],
+                ]
+            )
+            ->assertJsonCount($events->count(), 'data')
+            ->assertJson(
+                [
+                    'data' => $events->map(
+                        function (CalendarEvent $calendarEvent) {
+                            return array_merge(
+                                Arr::only(
+                                    $calendarEvent->toArray(),
+                                    [
+                                        'id',
+                                        'title',
+                                        'start_date',
+                                        'end_date',
+                                        'created_at',
+                                        'updated_at',
+                                    ]
+                                ),
+                                [
+                                    'days' => $calendarEvent->days->pluck('day')->all(),
+                                ]
+                            );
+                        }
+                    )
+                        ->toArray(),
+                ]
+            );
+    }
+}
