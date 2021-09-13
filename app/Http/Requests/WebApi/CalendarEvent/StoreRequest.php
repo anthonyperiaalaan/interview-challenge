076541f606f8a885d08eb7b90cac9cb81e004bda
anthony <anthonyperiaalaan@gmail.com>
2021-09-13 22:38:41 +0800

    feat: create calendar event validation

---
+++ app/Http/Requests/WebApi/CalendarEvent/StoreRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\WebApi\CalendarEvent;
+
+use App\Rules\DayOfWeekRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+            'start_date' => [
+                'required',
+                'date_format:Y-m-d',
+            ],
+            'end_date' => [
+                'required',
+                'date_format:Y-m-d',
+                'gte:start_date',
+            ],
+            'days' => [
+                'required',
+                'array',
+                'min:1',
+            ],
+            'days.*' => [
+                'required',
+                'integer',
+                new DayOfWeekRule(),
+            ],
+        ];
+    }
+}
