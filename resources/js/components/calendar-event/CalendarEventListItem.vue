ea92b897b55d7373da9495203feb33872a47d178
anthony <anthonyperiaalaan@gmail.com>
2021-09-14 17:43:27 +0800

    feat: show scheduled event on dates

--- resources/js/components/calendar-event/CalendarEventListItem.vue
+++ resources/js/components/calendar-event/CalendarEventListItem.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="flex items-center space-x-10 p-3 border-b">
-    {{ displayDate }}
+  <div
+    class="flex items-center space-x-10 p-3 border-b"
+    :class="{'bg-green-100': !!shownEvent}"
+    >
+    <span class="w-20">
+      {{ displayDate }}
+    </span>
+    <span>
+      {{ shownEvent ? shownEvent.title : '' }}
+    </span>
   </div>
 </template>
 
 <script>
 import { Moment } from 'moment'
+import { mapState } from 'vuex'
 
 export default {
   props: {
@@ -15,8 +24,14 @@ export default {
     },
   },
   computed: {
+    ...mapState(['events']),
     displayDate() {
       return this.date.format('D ddd')
+    },
+    shownEvent() {
+      const date = this.date.format('YYYY-MM-DD')
+
+      return this.events.find((e) => e.days.includes(this.date.day()) && date >= e.start_date && date <= e.end_date)
     },
   },
 }
