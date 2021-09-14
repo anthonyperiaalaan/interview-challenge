35af629c10aa2ff617aef4a5cd41a40a206b762f
anthony <anthonyperiaalaan@gmail.com>
2021-09-14 17:53:17 +0800

    test: delete existing events when creating calendar event

--- tests/Feature/CalendarEvent/CreateCalendarEventTest.php
+++ tests/Feature/CalendarEvent/CreateCalendarEventTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\CalendarEvent;
 
+use App\Models\CalendarEvent;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Carbon;
 use Tests\TestCase;
@@ -29,6 +30,44 @@ class CreateCalendarEventTest extends TestCase
             'calendar_events',
             Arr::except($payload, 'days')
         );
+
+        foreach ($payload['days'] as $day) {
+            $this->assertDatabaseHas(
+                'calendar_event_days',
+                [
+                    'calendar_event_id' => $response->json('data.id'),
+                    'day' => $day,
+                ]
+            );
+        }
+    }
+
+    public function testDeleteExistingEventsWhenCreatingCalendarEvent(): void
+    {
+        $existing = CalendarEvent::factory()
+            ->create();
+
+        $payload = [
+            'title' => $this->faker->text,
+            'start_date' => $startDate = $this->faker->date('Y-m-d', '-1 year'),
+            'end_date' => $this->faker->dateTimeBetween($startDate)->format('Y-m-d'),
+            'days' => $this->faker->randomElements(array_keys(Carbon::getDays()), 3),
+        ];
+
+        $response = $this->postJson('/web-api/calendar-event?delete-existing=1', $payload)
+            ->assertSuccessful()
+            ->assertJson(
+                [
+                    'data' => $payload,
+                ]
+            );
+
+        $this->assertDatabaseHas(
+            'calendar_events',
+            Arr::except($payload, 'days')
+        );
+
+        $this->assertDeleted($existing);
 
         foreach ($payload['days'] as $day) {
             $this->assertDatabaseHas(
