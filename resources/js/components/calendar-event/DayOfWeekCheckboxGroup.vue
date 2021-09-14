d9df5f5a0ef668d6c540e560ba2fc2341d1fef4a
anthony <anthonyperiaalaan@gmail.com>
2021-09-14 10:42:49 +0800

    feat: day of week checkbox group

---
+++ resources/js/components/calendar-event/DayOfWeekCheckboxGroup.vue
@@ -0,0 +1,25 @@
+<template>
+  <FormCheckboxGroup
+    :value="value"
+    :options="options"
+    @input="$emit('input', $event)"
+    />
+</template>
+
+<script>
+import FormCheckboxGroup from '../form/FormCheckboxGroup'
+import { daysOfWeek } from '@/utils/date'
+
+export default {
+  components: { FormCheckboxGroup },
+  props: {
+    value: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    options: () => daysOfWeek,
+  },
+}
+</script>
