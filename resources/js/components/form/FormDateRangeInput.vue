07b101a24a3c78450cc85041b39ba82f8d24563e
anthony <anthonyperiaalaan@gmail.com>
2021-09-14 10:41:16 +0800

    feat: date range input

---
+++ resources/js/components/form/FormDateRangeInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-date-picker
+    class="!w-full"
+    :value="value"
+    :placeholder="placeholder"
+    value-format="yyyy-MM-dd"
+    type="daterange"
+    range-separator="To"
+    start-placeholder="Start date"
+    end-placeholder="End date"
+    @input="onInput"
+    />
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Array,
+      default: null,
+    },
+    placeholder: {
+      type: String,
+      default: null,
+    },
+  },
+  methods: {
+    onInput(e) {
+      if (!e || e.length < 2) {
+        this.$emit('input', [null, null])
+        return
+      }
+      this.$emit('input', e)
+    },
+  },
+}
+</script>
