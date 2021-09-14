a5e59e204c2b2a656bcc5dedae05bfb33e2785f9
anthony <anthonyperiaalaan@gmail.com>
2021-09-14 11:51:18 +0800

    feat: setup notifications

--- resources/js/plugins/element-ui.js
+++ resources/js/plugins/element-ui.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import {
   DatePicker,
   Button,
+  Notification,
 } from 'element-ui'
 import lang from 'element-ui/lib/locale/lang/en'
 import locale from 'element-ui/lib/locale'
@@ -9,3 +10,5 @@ import locale from 'element-ui/lib/locale'
 locale.use(lang)
 Vue.component(DatePicker.name, DatePicker)
 Vue.component(Button.name, Button)
+
+Vue.prototype.$notify = Notification
